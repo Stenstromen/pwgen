8aea8f623a3156af9f896cdcd3f8615d4479ea7a
Stenstromen <kontakt@filipstenstrom.se>
2025-12-15 17:01:56 +0100

    web-vitals cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import sendToGoogleAnalytics from "./sendToGoogleAnalytics";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,5 +9,3 @@ const root = ReactDOM.createRoot(
 root.render(
     <App />
 );
-
-reportWebVitals(sendToGoogleAnalytics);
